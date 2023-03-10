19c590ed3abd9ab2dde50bc0c878f269ca78134a
Mariya <mariyatelyuk@gmail.com>
2023-03-10 11:57:10 -0500

    Starting

---
+++ src/test/java/com/cydeo/ticketingprojectmvc/TicketingProjectMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.ticketingprojectmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingProjectMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
