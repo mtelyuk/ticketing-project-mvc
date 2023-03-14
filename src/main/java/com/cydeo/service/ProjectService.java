cd146f5a9ce9cf317aca312139297b543310e037
Mariya <mariyatelyuk@gmail.com>
2023-03-14 16:52:02 -0400

    Part3

---
+++ src/main/java/com/cydeo/service/ProjectService.java
@@ -0,0 +1,9 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.service.CrudService;
+
+import javax.sql.rowset.CachedRowSet;
+
+public interface ProjectService extends CrudService<ProjectDTO,String> {
+}
